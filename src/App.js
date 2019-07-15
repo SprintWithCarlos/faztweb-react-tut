2b343c9bedb2e431bbb51a2028c9230f22b64f10
Carlos Marchena <sprintwithcarlos@pm.me>
2019-07-15 19:10:16 +0200

    End of first part: props and states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+
+import "./App.css";
+function Helloworld(props) {
+  return (
+    <div id="numberone">
+      <h1>
+        Hello {props.myname} {props.mylastname}
+      </h1>
+    </div>
+  );
+}
+class Newhelloworld extends React.Component {
+  state = {
+    show: true
+  };
+  toggleButton = () => {
+    this.setState({ show: !this.state.show });
+  };
+  render() {
+    return this.state.show ? (
+      <div>
+        <h1>
+          Hello {this.props.myname} {this.props.mylastname}
+        </h1>
+        <button onClick={this.toggleButton}>Toggle Button</button>
+      </div>
+    ) : (
+      <div>
+        <p> No show</p>
+        <button onClick={this.toggleButton}>Toggle Show</button>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div>
+        This is my component:{" "}
+        <Helloworld myname="Carlos" mylastname="Marchena" />{" "}
+      </div>
+      <div>
+        The new component{" "}
+        <Newhelloworld myname="Carlos" mylastname="Marchena" />
+      </div>
+    </Fragment>
   );
 }
 
